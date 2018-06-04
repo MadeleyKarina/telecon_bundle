efdc24b2b0aeb697fa4120ef92491ed7df077f99
Madeley Karina Coaquira Congona <m.coaquira@cgv.tugraz.at>
2018-06-03 22:49:47 -0300

    Service: create bundles

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,50 @@ import './App.css';
 
 class App extends Component {
   state = {
-    response: ''
+    response: []
   };
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res.bundles }))
       .catch(err => console.log(err));
   }
-
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
-
+    console.log(body);
     if (response.status !== 200) throw Error(body.message);
 
     return body;
   };
   render() {
+    const allBundles = this.state.response.map(bundle => {
+      return(
+        <div className = 'col-md-4'>
+          <div className='card mt-4' > 
+            <div className = 'card-body'>
+              <p>{bundle.services.map(service => {return <div><mark>{service}</mark></div>})}</p> 
+              <div className = 'card-header'>
+                Price : 
+                <span className = 'badge badge-danger ml-2'>
+                 {bundle.price} 
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>  )}
+    )
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        <div className= "container"> 
+          <div className = "row mt-4">
+            {allBundles}
+          </div>
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ function findEdges(idService, bundle, connectionValue){
   numberService = numberService.join("");
   var typeService = idService.replace(/[0-9]/g, '');
   var copyBundle = clone(bundle);
-  copyBundle.services.push(idService);
+  copyBundle.services.push(service[typeService][numberService].name);
   copyBundle.price = copyBundle.price + service[typeService][numberService].price + connectionValue;
   bundles.push(copyBundle);
   for(var i = 0; i < service[typeService][numberService].edges.length; i++){
@@ -25,25 +25,28 @@ function findEdges(idService, bundle, connectionValue){
   }
 }
 
-findEdges("bb1",bundle,connectionValue);
-console.log(bundles.length);
+findEdges("bb0",bundle,connectionValue);
+bundles.sort(function(a, b) {
+  return a.price -b.price;
+});
+
 console.log('///////////////');
 console.log(bundles);
 console.log('///////////////');
-var j;
-for(i = 0; i < service.bb.length; i++){
-  j=0;
-  while(service.bb[i].edges.length != 0 && j < service.bb[i].edges.length){
-    var bundle = { services : [] ,
-      price : 0
-    };
-    bundle.services.push(service.bb[i].id,service.bb[i].edges[j].id);
-    bundle.price = bundle.price + service.bb[i].edges[j].extraPrice + service.bb[i].price;
-    var copyBundle = clone(bundle);
-    bundles.push(copyBundle);
-    j++;
-  }
-}//console.log('bundlessssss');
+// var j;
+// for(i = 0; i < service.bb.length; i++){
+//   j=0;
+//   while(service.bb[i].edges.length != 0 && j < service.bb[i].edges.length){
+//     var bundle = { services : [] ,
+//       price : 0
+//     };
+//     bundle.services.push(service.bb[i].id,service.bb[i].edges[j].id);
+//     bundle.price = bundle.price + service.bb[i].edges[j].extraPrice + service.bb[i].price;
+//     var copyBundle = clone(bundle);
+//     bundles.push(copyBundle);
+//     j++;
+//   }
+// }//console.log('bundlessssss');
 //console.log(bundles);
 
 const express = require('express');
@@ -51,8 +54,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: bundles});
+//app.get('/api/hello', (req, res) => {
+app.get('/api/hello', function(req, res, next) {
+  res.send({ bundles});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+module.exports = app;
