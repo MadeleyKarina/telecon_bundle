cb98c2c43dd875c097dbd8c9634c7c10f2fe50de
Madeley Karina Coaquira Congona <m.coaquira@cgv.tugraz.at>
2018-06-04 22:25:58 -0300

    Add function to sort and list all services

--- server.js
+++ server.js
@@ -8,22 +8,41 @@ var bundles = [];
 var bundle = { services : [] ,
   price : 0
 };
-
-
-function findEdges(idService, bundle, connectionValue){
+function parseId(idService){
   var numberService = idService.match(/\d/g);
   numberService = numberService.join("");
   var typeService = idService.replace(/[0-9]/g, '');
+ return {number: numberService, type: typeService}
+}
+function findEdges(idService, bundle, connectionValue){
+  const{number, type} = parseId(idService);
   var copyBundle = clone(bundle);
-  copyBundle.services.push(service[typeService][numberService].name);
-  copyBundle.price = copyBundle.price + service[typeService][numberService].price + connectionValue;
+  copyBundle.services.push(service[type][number].name);
+  copyBundle.price = copyBundle.price + service[type][number].price + connectionValue;
   bundles.push(copyBundle);
-  for(var i = 0; i < service[typeService][numberService].edges.length; i++){
-    console.log(service[typeService][numberService].edges[i].id,service[typeService][numberService].edges.length);
-    var addConnectValue = service[typeService][numberService].edges[i].extraPrice;
-    findEdges(service[typeService][numberService].edges[i].id,copyBundle,addConnectValue);
+  for(var i = 0; i < service[type][number].edges.length; i++){
+    console.log(service[type][number].edges[i].id,service[type][number].edges.length);
+    var addConnectValue = service[type][number].edges[i].extraPrice;
+    findEdges(service[type][number].edges[i].id,copyBundle,addConnectValue);
   }
 }
+function sortByPrice(bundles){
+  bundles.sort(function(a, b) {
+      return a.price -b.price;
+  });
+  return bundles;
+}
+function listBundles(typeService){
+  for(i = 0; i < service[typeService].length; i++){
+    var connectionValue = 0;
+    findEdges(service[typeService][i].id,bundle,connectionValue);
+  }
+  bundles = sortByPrice(bundles);
+}
+// tService = 'bb';
+// console.log('length',service[tService].length)
+// listBundles('bb');
+// console.log(bundles)
 var connectionValue = 0;
 // for(i = 0; i < service.bb.length; i++){
 //   var connectionValue = 0;
@@ -62,7 +81,8 @@ const port = process.env.PORT || 5000;
 app.get('/api/hello', function(req, res, next) {
   const serviceSearch = req.query.serviceSearch;
   console.log(serviceSearch);
-  findEdges(service[serviceSearch][0].id,bundle,connectionValue);
+  listBundles(serviceSearch);
+  //findEdges(service[serviceSearch][0].id,bundle,connectionValue);
   console.log(bundles)
   res.send({bundles});
 });
